9a965f5c2b33c60621f9503a855895f3fd227ac1
Paul-Antoine Fruchtenreich <paul-antoine.fruchtenreich@ens.psl.eu>
2025-04-28 20:34:13 +0100

    add functions for DAPT loading

--- src/data_processing/loading.py
+++ src/data_processing/loading.py
@@ -29,3 +29,30 @@ def load_iob_data(path):
         labels.append(current_labels)
 
     return sentences, labels
+
+
+def load_text_from_iob_mlm(path):
+    texts = []
+    current_tokens = []
+
+    with open(path, encoding="utf-8") as f:
+        next(f)  # skip header
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                if current_tokens:
+                    text = " ".join(current_tokens)
+                    texts.append(text)
+                    current_tokens = []
+                continue
+
+            parts = line.split("\t")
+            token = parts[0]
+            current_tokens.append(token)
+
+    # Catch last sentence
+    if current_tokens:
+        text = " ".join(current_tokens)
+        texts.append(text)
+
+    return texts
